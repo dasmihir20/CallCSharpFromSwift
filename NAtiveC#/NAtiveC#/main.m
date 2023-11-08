dec578a4811ae1e160ec05cece3f97dc6090a73c
Mihir Das <mihir_das@starkey.com>
2023-11-08 22:49:44 +0530

    Trying out call Native method call from C# side, but having a crash

--- NAtiveC#/NAtiveC#/main.m
+++ NAtiveC#/NAtiveC#/main.m
@@ -8,15 +8,50 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+
+
+typedef void* GCHandle;
+
 void SayHello(void);
+//void SumCall(void);
+int Add(int a, int b);
+char* SumString(char const * a, char const * b);
+
+GCHandle StructReturn(void);
+void UpdateFirstName(GCHandle object, char const * fName);
+
+
+typedef void(*callbackFunction)(int value);
+void set_native_callback (callbackFunction function);
+
+void callback (int value)
+{
+    printf("value: %i\n", value);
+}
+
+void initialize(void)
+{
+    set_native_callback (&callback);
+}
 
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
+    char *path = strdup ([[[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Frameworks"] UTF8String]);
+    setenv ("DOTNET_LIBRARY_ASSEMBLY_PATH", path, 1);
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
+       
+       // SayHello();
+        //SumCall();
+        int a = Add(8,9);
+        char* result = SumString("Mihir", "Das");
         
-        SayHello();
+        GCHandle handle = StructReturn();
+        UpdateFirstName(handle, "Rahul");
+        initialize();
+        
+        NSLog(@"Sum: %d result: %s", a, result);
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
